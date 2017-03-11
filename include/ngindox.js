c673d675e1a893d331948bedfa73f15f2cc82f13
Karl Isenberg <karl@mesosphere.io>
2017-03-10 22:30:26 -0800

    Clean up css and js

--- include/ngindox.js
+++ include/ngindox.js
@@ -1,23 +1,11 @@
-// requires jquery-3.1.1
-window.Ngindox = {
-
-  init: function() {
-    $('.toggleEndpointList', this.el).click(function() {
-      var anchor = this.getAttribute('data-id');
-      if (anchor) {
-        Ngindox.toggleEndpointListForResource(anchor);
-      }
-    });
-  },
-
-  toggleEndpointListForResource: function(resource) {
-    $('#routes-' + resource).slideToggle("slow");
-  },
-
-  callDocs: function(fnName, e) {
-    e.preventDefault();
-    Ngindox[fnName](e.currentTarget.getAttribute('data-id'));
-  }
-
-}
-$(document).ready(Ngindox.init);
+// Ngindox
+// requires jquery
+window.NgindoxInit = function() {
+  $('.toggleEndpointList').click(function() {
+    var resource = this.getAttribute('data-id');
+    if (resource) {
+      $('#routes-' + resource).slideToggle("slow");
+    }
+  });
+};
+$(document).ready(NgindoxInit);
